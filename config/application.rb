495b96162cedd32e6a120e843a7482de5517ed46
Tony Coconate <me@tonycoconate.com>
2015-02-17 23:07:27 -0600

    Rack CORS

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module Contacts
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Rack Middleware for CORS (https://github.com/cyu/rack-cors)
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins "*"
+        resource "*", headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
+      end
+    end
   end
 end
